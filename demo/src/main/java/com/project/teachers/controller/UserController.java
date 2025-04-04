c72ec0f1e591d695ae8a94059a24c36752ab3999
3232kk <3232kk@gmail.com>
2025-04-04 20:39:01 +0900

    2025/4/4 로그인/회원가입 완료

--- demo/src/main/java/com/project/teachers/controller/UserController.java
+++ demo/src/main/java/com/project/teachers/controller/UserController.java
@@ -3,11 +3,14 @@ package com.project.teachers.controller;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.project.teachers.entity.Role;
 import com.project.teachers.entity.User;
 import com.project.teachers.service.MenuService;
 import com.project.teachers.service.UserService;
@@ -18,6 +21,9 @@ public class UserController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	//user처음 테스트용
 		@GetMapping("/usertest.me")
@@ -31,5 +37,27 @@ public class UserController {
 			
 			 return "select"; 
 		 }
+		
+		
+		
+		//카네즈
+		@PostMapping("/register")
+		// register 로 이동할 때 user 정보를 파라미터로 가진 채 이동 
+		public String register(@ModelAttribute User user) {
+			
+			String userPassword = user.getPwd();
+			System.out.println("!!!!!!!!!!userPassword: "+userPassword);
+			
+			//user.setRole(Role.MEMBER); //멤버로 지정 
+			
+			String passwordEncoded = passwordEncoder.encode(userPassword); // 암호화
+			
+			user.setPwd(passwordEncoded); // 암호화된 패스워드 담김 
+			
+			userService.insertUser(user); // 유저 정보를 데이터베이스에 저장 
+			
+			// 다시 로그인 화면으로 리다이렉팅
+			return "redirect:/loginPage";
+		}
 
 }
